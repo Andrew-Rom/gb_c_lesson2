6dd53a1fbe8466e507d0535a63744fd6e42fa5f1
And-Rom <you@example.com>
2022-10-04 22:15:34 +0300

    Task 13 Find third number

---
+++ Task13_ThirdNum/Program.cs
@@ -0,0 +1,17 @@
+﻿/*Задача 13
+Напишите программу, которая 
+выводит третью цифру заданного числа 
+или сообщает, что третьей цифры нет.*/
+
+Console.Write("Введите число: ");
+string value = Console.ReadLine();
+int valueLen = value.Length;
+if (valueLen >= 3)
+{
+    string thirdNum = value.Substring(2, 1);
+    Console.WriteLine("Третья цифра этого числа: " + thirdNum);
+}
+else
+{
+    Console.WriteLine("Третьей цифры нет");
+}
