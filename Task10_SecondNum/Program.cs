fbbc4d687147cfc420be4a90859f9d3efcd34318
And-Rom <you@example.com>
2022-10-04 21:43:50 +0300

    Task 10 Find second number

---
+++ Task10_SecondNum/Program.cs
@@ -0,0 +1,18 @@
+﻿/* Задача 10
+Напишите программу, которая
+принимает на вход трёхзначное число 
+и на выходе показывает вторую цифру этого числа. */
+
+Console.Write("Введите трехзначное число: ");
+string valueStr = Console.ReadLine();
+int valueNum = int.Parse(valueStr);
+
+if (valueNum > 99 && valueNum < 1000)
+{
+    int secondNum = (valueNum % 100) / 10;
+    Console.WriteLine("Вторая цифра этого числа: " + secondNum);
+}
+else
+{
+    Console.WriteLine("Введено некорректное число");
+}
